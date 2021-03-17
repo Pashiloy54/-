f3fe92a80f1dca44d22e7da9f2ae2f280c5df849
Pashiloy54 <80844565+Pashiloy54@users.noreply.github.com>
2021-03-17 23:47:37 +0300

    Add files via upload

---
+++ main.pas
@@ -0,0 +1,17 @@
+﻿begin
+  var first, two: real; 
+  var answer: string;
+  write('Введите первое число: '); readln(first);
+  write('Введите второе число: '); readln(two);
+  write('Знак: '); readln(answer);
+  if answer = '+' then
+    write(first + two);
+  if answer = '-' then
+    write(first - two);
+  if answer = '*' then
+    write(first * two);
+  if answer = '/' then
+    write(first / two)
+  else
+    write('НЕВЕРНЫЙ ЗНАК!!!')
+end.
